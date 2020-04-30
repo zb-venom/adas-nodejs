2fe55882d1328572069bf1c346d1ccb1c55e6629
ZB Tyoma <zb.tyoma@gmail.com>
2020-04-30 16:58:23 +0700

    master

--- controllers/appController.js
+++ controllers/appController.js
@@ -245,7 +245,8 @@ exports.postApiAuth = async (req, res) => {
         request('http://ulogin.ru/token.php?token='+req.body.token+'&host=https://adas-tusur.herokuapp.com/', function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log(body)
-                console.log(body.uid+' network = '+body.network)
+                data = body;
+                console.log(data.uid+' network = '+body.network)
                 res.cookie('uid', body.uid);
               }            
               /*var user = await usersSchema.findOne({$or: [{vk_uid: data.uid}, {google_uid: data.uid}, {ya_uid: data.uid}]});
