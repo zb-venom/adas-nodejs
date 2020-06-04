5fa7ef94e6832af2978ca326a99efc1138646fb2
ZB Tyoma <zb.tyoma@gmail.com>
2020-06-04 11:52:53 +0700

    master

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -244,7 +244,7 @@ exports.editUser = async (req, res) => {
     var status = await check.check(req, res);
     if (!status.online) { res.redirect('/'); return; }
     if (!status.admin) { res.redirect('/lk'); return; }
-    /*if (req.body.about && req.body.login && req.body._id && req.body.email 
+    if (req.body.about && req.body.login && req.body._id && req.body.email 
         && req.body.type && req.body.phone && req.body.code) {            
         await usersSchema.findByIdAndUpdate(req.body._id, {
             about: req.body.about,
@@ -263,8 +263,8 @@ exports.editUser = async (req, res) => {
         })
         console.log('Пользователь (_id: '+req.cookies._id+') изменил пользователя '+req.body.login+' (_id: '+req.body._id+').');
         res.redirect('/users')
-    }*/
-    else { console.log(req.body.new_password); console.log(req.body.login); res.redirect('/users')  }
+    }
+    else { res.redirect('/users')  }
 
 }
 
